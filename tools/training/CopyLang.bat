783baba285f844567a292ec12a6dfeb43040e672
Christian Mogensen <Christianm@superoffice.com>
2023-03-13 16:28:53 +0100

    Fix paths

--- tools/training/CopyLang.bat
+++ tools/training/CopyLang.bat
@@ -1,8 +1,8 @@
 @echo off
-if "%1" == "" echo Need language code
+if "%1" == "" echo Need language code: en, da, de, nl, no, se
 if "%1" == "" exit /b
-if not exist en\text echo Need to install 'en' language first
-if not exist en\text exit /b
+if "%1" neq "en" if not exist artifacts\en\text echo Need to install 'en' language first
+if "%1" neq "en" if not exist artifacts\en\text exit /b
 
 if not exist glossary-en-nb.tmx GenGlossary\GenGlossary.exe gloss-md.tsv allresources.align
 
@@ -11,13 +11,12 @@ set ARTIF=artifacts\%1
 if exist %ARTIF% rmdir %ARTIF% /q /s
 if not exist artifacts mkdir artifacts
 if not exist %ARTIF% mkdir %ARTIF%
-robocopy "\\norfilepub\RnD\Semantix Ink\Help\10.1\Azure-Update-10.1.8-20221114\%1\CRM\UserHelp" %ARTIF%\userhelp *.htm* /s
-robocopy "\\norfilepub\RnD\Semantix Ink\Help\10.1\Azure-Update-10.1.8-20221114\%1\CRM\WebHelpAdmin" %ARTIF%\webhelpadmin *.htm* /s
+robocopy "\\norfilepub\RnD\Semantix Ink\Help\10.1\Azure-Update-10.1.8-20221114\%1\CRM\UserHelp" %ARTIF%\userhelp *.htm* /s /NP 
+robocopy "\\norfilepub\RnD\Semantix Ink\Help\10.1\Azure-Update-10.1.8-20221114\%1\CRM\WebHelpAdmin" %ARTIF%\webhelpadmin *.htm* /s /NP 
 
 set LANG=%1
-if "%LANG%" =="no" (
-    set LANG=NB
-)
+if "%LANG%" =="no" set LANG=nb
+if "%LANG%" =="se" set LANG=sv
 
 cd %ARTIF%
 del /s access_cook*
@@ -26,12 +25,13 @@ del /s index*
 if exist text rmdir /s text
 if not exist text mkdir text
 
-powershell ..\make-text.ps1 %LANG%
+powershell ..\..\make-text.ps1 %LANG%
 
 if NOT EXIST text cd ..
 if NOT EXIST text cd ..
 if EXIST text cd ..
-if EXIST artifacts cd ..
+if NOT EXIST artifacts cd ..
+if NOT EXIST artifacts cd ..
 
 if "%LANG%" =="en" exit /b
 
